81158394f46541be16cbf1fd087f214a734e35f6
unknown <kalyanij967@gmail.com>
2024-04-15 13:12:11 +0530

    added login and logout functionalities

--- sapience-client/src/main.jsx
+++ sapience-client/src/main.jsx
@@ -4,6 +4,16 @@ import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Router/Router.jsx'
+import { Auth0Provider } from '@auth0/auth0-react';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <Auth0Provider
+    domain="dev-cxzgpovs56dgdxkm.us.auth0.com"
+    clientId="GDZrHjJ1LW47ozCDLLi7IKIcdAWT2hWJ"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <RouterProvider router={router} />
+  </Auth0Provider>
 )
